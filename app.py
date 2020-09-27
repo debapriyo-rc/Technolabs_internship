b06e3605da31e9a1641200263a396d973449c985
Debapriyo Roy Chowdhury <68781324+debapriyo-rc@users.noreply.github.com>
2020-09-27 16:24:44 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -8,52 +8,35 @@ def predict_forest(LIMIT_BAL,EDUCATION,MARRIAGE,AGE,PAY_1,BILL_AMT1,BILL_AMT2,BI
     return prediction
 
 def main():
-    st.title("Streamlit Tutorial")
+    st.title("Streamlit")
     html_temp = """
     <div style="background-color:#025246 ;padding:10px">
-    <h2 style="color:white;text-align:center;">Forest Fire Prediction ML App </h2>
+    <h2 style="color:white;text-align:center;">Credit Card App </h2>
     </div>
     """
     st.markdown(html_temp, unsafe_allow_html=True)
-    LIMIT_BAL = st.text_input("Limit_bal","Type Here")
-    EDUCATION = st.text_input("Education","Type Here")
-    MARRIAGE = st.text_input("Marraige","Type Here")
-    AGE = st.text_input("Age","Type Here")
-    PAY_1 = st.text_input("Pay_1","Type Here")
-    BILL_AMT1 = st.text_input("Bill_amt1","Type Here")
-    BILL_AMT2 = st.text_input("Bill_amt2","Type Here")
-    BILL_AMT3 = st.text_input("Bill_amt3","Type Here")
-    BILL_AMT4 = st.text_input("Bill_amt4","Type Here")
-    BILL_AMT5 = st.text_input("Bill_amt5","Type Here")
-    BILL_AMT6 = st.text_input("Bill_amt6","Type Here")
-    PAY_AMT1  = st.text_input("Pay_amt1","Type Here")
-    PAY_AMT2  = st.text_input("Pay_amt2","Type Here")
-    PAY_AMT3  = st.text_input("Pay_amt3","Type Here")
-    PAY_AMT4  = st.text_input("Pay_amt4","Type Here")
-    PAY_AMT5  = st.text_input("Pay_amt5","Type Here")
-    PAY_AMT6  = st.text_input("Pay_amt6","Type Here")
+    LIMIT_BAL = st.text_input("Limit_bal")
+    EDUCATION = st.text_input("Education")
+    MARRIAGE = st.text_input("Marraige")
+    AGE = st.text_input("Age")
+    PAY_1 = st.text_input("Pay_1")
+    BILL_AMT1 = st.text_input("Bill_amt1")
+    BILL_AMT2 = st.text_input("Bill_amt2")
+    BILL_AMT3 = st.text_input("Bill_amt3")
+    BILL_AMT4 = st.text_input("Bill_amt4")
+    BILL_AMT5 = st.text_input("Bill_amt5")
+    BILL_AMT6 = st.text_input("Bill_amt6")
+    PAY_AMT1  = st.text_input("Pay_amt1")
+    PAY_AMT2  = st.text_input("Pay_amt2")
+    PAY_AMT3  = st.text_input("Pay_amt3")
+    PAY_AMT4  = st.text_input("Pay_amt4")
+    PAY_AMT5  = st.text_input("Pay_amt5")
+    PAY_AMT6  = st.text_input("Pay_amt6")
 
-
-
-#     safe_html="""
-#       <div style="background-color:#F4D03F;padding:10px >
-#        <h2 style="color:white;text-align:center;"> Your forest is safe</h2>
-#        </div>
-#     """
-#     danger_html="""
-#       <div style="background-color:#F08080;padding:10px >
-#        <h2 style="color:black ;text-align:center;"> Your forest is in danger</h2>
-#        </div>
-#     """
 
     if st.button("Predict"):
         output=predict_forest(LIMIT_BAL,EDUCATION,MARRIAGE,AGE,PAY_1,BILL_AMT1,BILL_AMT2,BILL_AMT3,BILL_AMT4,BILL_AMT5,BILL_AMT6,PAY_AMT1,PAY_AMT2,PAY_AMT3,PAY_AMT4,PAY_AMT5,PAY_AMT6)
-        st.success('The probability of fire taking place is {}'.format(output))
-
-#         if output > 0.5:
-#             st.markdown(danger_html,unsafe_allow_html=True)
-#         else:
-#             st.markdown(safe_html,unsafe_allow_html=True)
+        st.success('The value is {}'.format(output))
 
 if __name__=='__main__':
     main()
